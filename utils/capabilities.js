c6b46bebb7f4002390d623441cfd86c05a4e2a59
AmruthBB <amruthd@blubirch.com>
2025-10-10 17:42:15 +0530

    new changes1

--- utils/capabilities.js
+++ utils/capabilities.js
@@ -3,13 +3,24 @@ module.exports = {
     path: '/wd/hub',
     port: 4723,
     capabilities: {
-        platformName: 'Android',
-        deviceName: 'RZCX20C3RVT', // your emulator/device name
-        app: '../testdata/RIMS2-1.0.104.apk', // full path to your APK
-        automationName: 'UiAutomator2',
-        appWaitActivity: '*', // wait for any activity
-        newCommandTimeout: 300,
-        autoGrantPermissions: true // <-- auto-accepts all runtime permissions
+
+        //Local
+        // platformName: 'Android',
+        // deviceName: 'RZCX20C3RVT', // your emulator/device name
+        // app: '../testdata/RIMS2-1.0.104.apk', // full path to your APK
+        // automationName: 'UiAutomator2',
+        // appWaitActivity: '*', // wait for any activity
+        // newCommandTimeout: 300,
+        // autoGrantPermissions: true // <-- auto-accepts all runtime permissions
         
+        // BrowserStack
+        platformName: 'Android',
+        deviceName: 'Android Emulator',
+        // platformVersion: '9.0',
+        app: '../testdata/RIMS2-1.0.104.apk', // replace with your BrowserStack app URL
+        automationName: 'UiAutomator2',
+        appWaitActivity: '*',
+        newCommandTimeout: 300,
+        autoGrantPermissions: true   
     }
 };
